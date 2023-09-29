b8288c0924992f5b5014beee965a1d5f7ac391eb
isreallee82 <isreallee82@gmail.com>
2023-09-29 02:59:45 +0100

    classes

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,13 @@
 export default function uploadPhoto(filename) {
   return Promise.reject(new Error(`${filename} cannot be processed`));
 }
+
+// export const response = await uploadPhoto().catch((err) => { err = new Error(`${filename} cannot be processed`); });
+
+// async function uploadPhoto() {
+//     try {
+//       const z = await Promise.reject(30);
+//     } catch (e) {
+//       console.error(e); // 30
+//     }
+//   }
